afd30913d869ff635a61a0b89fc8fa29b9971db5
berkc <b.cal.2193@gmail.com>
2022-07-21 19:04:20 +0200

    21.07 API

---
+++ src/test/java/z_deneme/z_deneme_get_object_mapper/Get1ObjectMapper.java
@@ -0,0 +1,82 @@
+package z_deneme.z_deneme_get_object_mapper;
+
+import base_Urls.JsonPlaceHolderBaseUrl;
+import io.restassured.response.Response;
+import org.junit.Test;
+import pojos.JsonPlaceHolderPojo;
+import utils.JsonUtil;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static io.restassured.RestAssured.*;
+import static org.junit.Assert.assertEquals;
+
+public class Get1ObjectMapper extends JsonPlaceHolderBaseUrl {
+    /*
+        Given
+           https://jsonplaceholder.typicode.com/todos/198
+        When
+         I send GET Request to the URL
+      Then
+         Status code is 200
+         And response body is like {
+                               "userId": 10,
+                               "id": 198,
+                               "title": "quis eius est sint explicabo",
+                               "completed": true
+                             }
+     */
+
+
+    @Test
+    public void get1ObjectMapper() {
+
+        spec.pathParams("first", "todos", "second", 198);
+
+        String expectedData = "{\n" +
+                " \"userId\": 10,\n" +
+                " \"id\": 198,\n" +
+                " \"title\": \"quis eius est sint explicabo\",\n" +
+                " \"completed\": true\n" +
+                " }";
+
+        Map<String, Object> expectedDataMap = JsonUtil.convertJsonToJavaObject(expectedData, HashMap.class);
+
+        Response response = given().spec(spec).when().get("/{first}/{second}");
+        Map<String, Object> actualBodyMap = JsonUtil.convertJsonToJavaObject(response.asString(), HashMap.class);
+
+        assertEquals(200, response.getStatusCode());
+        assertEquals(expectedDataMap.get("userId"), actualBodyMap.get("userId"));
+        assertEquals(expectedDataMap.get("title"), actualBodyMap.get("title"));
+        assertEquals(expectedDataMap.get("completed"), actualBodyMap.get("completed"));
+    }
+
+
+    @Test
+    public void get2ObjectMapper() {
+        spec.pathParams("first", "todos", "second", 198);
+
+        String expectedData = "{\n" +
+                " \"userId\": 10,\n" +
+                " \"id\": 198,\n" +
+                " \"title\": \"quis eius est sint explicabo\",\n" +
+                " \"completed\": true\n" +
+                " }";
+
+        JsonPlaceHolderPojo expectedDataPojo = JsonUtil.convertJsonToJavaObject(expectedData, JsonPlaceHolderPojo.class);
+
+        Response response = given().spec(spec).when().get("/{first}/{second}");
+
+        JsonPlaceHolderPojo actualBodyPojo = JsonUtil.convertJsonToJavaObject(response.asString(), JsonPlaceHolderPojo.class);
+
+        assertEquals(200,response.getStatusCode());
+        assertEquals(expectedDataPojo.getUserId(),actualBodyPojo.getUserId());
+        assertEquals(expectedDataPojo.getTitle(),actualBodyPojo.getTitle());
+        assertEquals(expectedDataPojo.getCompleted(),actualBodyPojo.getCompleted());
+
+
+    }
+
+
+}
---
+++ src/test/java/z_deneme/z_deneme_get_object_mapper/Get2ObjectMapper.java
@@ -0,0 +1,66 @@
+package z_deneme.z_deneme_get_object_mapper;
+
+import base_Urls.HerOkuAppBaseUrl;
+import io.restassured.response.Response;
+import org.junit.Test;
+import org.testng.asserts.SoftAssert;
+import pojos.BookingPojo;
+import utils.JsonUtil;
+
+import static io.restassured.RestAssured.*;
+
+public class Get2ObjectMapper extends HerOkuAppBaseUrl {
+     /*
+        Given
+               https://restful-booker.herokuapp.com/booking/22
+        When
+            I send GET Request to the URL
+      Then
+            Status code is 200
+           {
+                "firstname": "Oliver",
+                "lastname": "Smith",
+                "totalprice": 100,
+                "depositpaid": true,
+                "bookingdates": {
+                    "checkin": "2022-07-18",
+                    "checkout": "2022-07-19"
+                },
+                "additionalneeds": "Breakfast"
+            }
+     */
+
+    @Test
+    public void get1ObjectMapper() {
+        spec.pathParams("first", "booking", "second", 22);
+
+        String expectedData = "{\n" +
+                "                \"firstname\": \"Oliver\",\n" +
+                "                \"lastname\": \"Smith\",\n" +
+                "                \"totalprice\": 100,\n" +
+                "                \"depositpaid\": true,\n" +
+                "                \"bookingdates\": {\n" +
+                "                    \"checkin\": \"2022-07-18\",\n" +
+                "                    \"checkout\": \"2022-07-19\"\n" +
+                "                },\n" +
+                "                \"additionalneeds\": \"Breakfast\"\n" +
+                "            }";
+
+        BookingPojo expectedDataPojo = JsonUtil.convertJsonToJavaObject(expectedData, BookingPojo.class);
+
+        Response response = given().spec(spec).when().get("/{first}/{second}");
+
+        BookingPojo actualDataPojo = JsonUtil.convertJsonToJavaObject(response.asString(), BookingPojo.class);
+
+        SoftAssert softAssert = new SoftAssert();
+
+        softAssert.assertEquals(expectedDataPojo.getFirstname(),actualDataPojo.getFirstname());
+        softAssert.assertEquals(expectedDataPojo.getLastname(),actualDataPojo.getLastname());
+        softAssert.assertEquals(expectedDataPojo.getTotalprice(),actualDataPojo.getTotalprice());
+        softAssert.assertEquals(expectedDataPojo.getDepositpaid(),actualDataPojo.getDepositpaid());
+        softAssert.assertEquals(expectedDataPojo.getBookingdates().getCheckin(),actualDataPojo.getBookingdates().getCheckin());
+        softAssert.assertEquals(expectedDataPojo.getBookingdates().getCheckout(),actualDataPojo.getBookingdates().getCheckout());
+    }
+
+
+}
